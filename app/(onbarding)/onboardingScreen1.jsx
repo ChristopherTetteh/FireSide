77c66b5e5a71cfb3b3311dfff0b6674831437d24
Henry Amos <amoshenry95@gmail.com>
2024-08-07 10:56:28 +0000

    OnboardingLayout Component Created,Onboarding Screens Added

--- app/(onbarding)/onboardingScreen1.jsx
+++ app/(onbarding)/onboardingScreen1.jsx
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native'
 import React from 'react'
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
+import OnboardingLayout from '../../components/OnboardingLayout'
 const OnboardingScreen1 = () => {
   return (
-    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-      <Text>OnboardingScreen1</Text>
-    </View>
+    <GestureHandlerRootView style={{flex:1}}>
+    <SafeAreaView style={{ flex:1, width:'100%'}}>
+   <OnboardingLayout
+       imageSource={images.onboardingImage1}
+       title="Best Tales of Kweku Ananse"
+       subtitle="Enjoy the best of African folklore stories"
+       activeDotIndex={0}
+       nextScreen="/onboardingScreen2"
+       skipScreen="/sign-up"
+   />
+    
+    </SafeAreaView>
+    </GestureHandlerRootView>
   )
 }
 
--- app/(onbarding)/onboardingScreen2.jsx
+++ app/(onbarding)/onboardingScreen2.jsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
+import {GestureHandlerRootView}  from 'react-native-gesture-handler'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react' 
+import { images } from '../../constants'
+import OnboardingLayout from '../../components/OnboardingLayout'
 
 const OnboardingScreen2 = () => {
   return (
-    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-      <Text>OnboardingScreen2</Text>
-    </View>
+    <GestureHandlerRootView style={{flex:1}}>
+    <SafeAreaView style={{ flex:1, width:'100%'}}>
+   <OnboardingLayout
+       imageSource={images.onboardingImage2}
+       title="Discover New Stories"
+       subtitle="Explore latest and trending stories"
+       activeDotIndex={1}
+       nextScreen="/onboardingScreen3"
+       skipScreen="/sign-up"
+   />
+    </SafeAreaView>
+    </GestureHandlerRootView>
   )
 } 
-
 export default OnboardingScreen2
