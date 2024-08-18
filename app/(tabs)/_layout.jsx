3e37ffdff41ab7bd18ab290adb229536768e20ee
Henry Amos <amoshenry95@gmail.com>
2024-08-18 15:42:49 +0000

    Fetched Username to Home Screen

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { Tabs,Redirect } from 'expo-router'
 import { icons } from '../../constants'
+import { colors } from '../../styles/globalStyles'
 
 const TabIcon=({color,icon,name,focused})=>{
     return(
@@ -22,17 +23,27 @@ export default function TabsLayout() {
   return (
     <>
     <Tabs
-      
+     screenOptions={{
+      tabBarShowLabel: false, // Hide default labels if you're adding custom ones
+      tabBarStyle: {
+        backgroundColor: colors.secondary, // Customize background
+        borderTopWidth: 0, // Remove border
+        elevation: 50, // Android shadow
+        shadowOpacity: 0.9,
+        shadowOffset: { width: 12, height: 6 },
+        shadowRadius: 30,
+      },
+    }}
     >
       <Tabs.Screen
         name="home"
         options={{
           title: 'Home',
           headerShown:false,
-          tabBarIcon:({color,focused})=>{
+          tabBarIcon:({icon,color,focused})=>{
              <TabIcon
              icon={icons.homeIcon}
-             color={color}
+             color={colors.primary}
              focused={focused}
              />
           }
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { View,Text, ScrollView,Image } from 'react-native';
+import { useGlobalContext } from '../../context/GlobalProvider'; // Import the global context
+import { StyleSheet } from 'react-native';
+import { colors } from '../../styles/globalStyles';
+import { wp,hp } from '../../utils/responsive';
+import { icons, images } from '../../constants';
+const Home = () => {
+  const { user, isLoggedIn } = useGlobalContext(); // Destructure user and isLoggedIn from global context
 
-export default function Home() {
   return (
-    <View>
-      <Text>Hi User</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
+    <ScrollView>
+{/* Header Part */}
+    <View style={styles.headerContainer}>
+      <View >
+        {isLoggedIn && user&& ( // Check if user is logged in and user data exists
+          <Text style={styles.welcomeText}>Hi, {user.username}</Text> // Display the username
+        )}
+      <Text>Welcome to Fire Side</Text>
+      </View>
+      <Image source={icons.profileIcon} style={{width:30,height:30}}/>
+        </View>
+    </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles= StyleSheet.create({
+  container: {
+    width: '100%',
+    flex: 1,
+    paddingHorizontal: wp(5),
+    paddingVertical: hp(5),
+    backgroundColor: colors.secondary,
+  },
+  headerContainer:{
+  flexDirection:'row',
+justifyContent:'space-between',
+  },
+  welcomeText: {
+    fontSize: wp(4),
+    fontFamily:'Poppins-Bold',
+    color: colors.textBalck,
+    textAlign: 'left',
+    marginBottom: hp(1),
+  },
+ 
+})
+export default Home;
